1369d2dc47a3cd1cc93b4d2b2b7d3c285c455b63
Erik Seliger <erikseliger@me.com>
2021-05-17 13:08:40 +0200

    Modernize plugin code and fix bug
    @babel/traverse has a bug in the ExportNamedDeclaration traverser, which is fixed by iterating over the program level nodes by hand. This was suggested by the a maintainer of the babel repo and he said it should also improve performance.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,40 +1,49 @@
 import pluginTester from 'babel-plugin-tester'
+
 import plugin from '.'
 
-const IMPORTS = `import { hot as _hot } from "react-hot-loader/root";\n`
+const IMPORTS = 'import { hot as _hot } from \'react-hot-loader/root\'\n'
 
 pluginTester({
     plugin,
+    pluginName: 'babel-plugin-transform-react-hot-loader-wrapper',
+    babelOptions: { filename: 'Page.tsx' },
     tests: [
         {
             title: 'const arrowfunc',
-            code: `export const A = () => 'a'`,
-            output: `${IMPORTS}export const A = _hot(() => 'a');`,
-            pluginOptions: { modulePattern: '', componentNamePattern: '^A$' },
+            code: 'export const A = () => \'a\'',
+            output: `${IMPORTS}export const A = _hot(() => 'a')`,
+            pluginOptions: { modulePattern: 'Page\\.tsx?', componentNamePattern: '^A$' },
         },
         {
             title: 'class',
-            code: `export class A extends React.Component {}`,
-            output: `${IMPORTS}export const A = _hot(class A extends React.Component {});`,
-            pluginOptions: { modulePattern: '', componentNamePattern: '^A$' },
+            code: 'export class A extends React.Component {}',
+            output: `${IMPORTS}export const A = _hot(class A extends React.Component {})`,
+            pluginOptions: { modulePattern: 'Page\\.tsx?', componentNamePattern: '^A$' },
         },
         {
             title: 'function',
-            code: `export function A(props) { return 'a'; }`,
-            output: `${IMPORTS}export const A = _hot(function A(props) {\n  return 'a';\n});`,
-            pluginOptions: { modulePattern: '', componentNamePattern: '^A$' },
+            code: 'export function A(props) { return \'a\'; }',
+            output: `${IMPORTS}export const A = _hot(function A(props) {\n    return 'a'\n})`,
+            pluginOptions: { modulePattern: 'Page\\.tsx?', componentNamePattern: '^A$' },
         },
         {
             title: 'ignored by componentNamePattern',
-            code: `export const X = () => 'a'`,
-            output: `export const X = () => 'a';`,
-            pluginOptions: { modulePattern: '', componentNamePattern: '^A$' },
+            code: 'export const X = () => \'a\'',
+            output: 'export const X = () => \'a\'',
+            pluginOptions: { modulePattern: 'Page\\.tsx?', componentNamePattern: '^A$' },
+        },
+        {
+            title: 'ignored by modulePattern',
+            code: 'export const A = () => \'a\'',
+            output: 'export const A = () => \'a\'',
+            pluginOptions: { modulePattern: 'Area\\.tsx?', componentNamePattern: '^A$' },
         },
         {
             title: 'multiple in same file',
-            code: `export const A = () => 'a'; export const B = () => 'b'`,
-            output: `${IMPORTS}export const A = _hot(() => 'a');\nexport const B = _hot(() => 'b');`,
-            pluginOptions: { modulePattern: '', componentNamePattern: '' },
+            code: 'export const A = () => \'a\'; export const B = () => \'b\'',
+            output: `${IMPORTS}export const A = _hot(() => 'a')\nexport const B = _hot(() => 'b')`,
+            pluginOptions: { modulePattern: 'Page\\.tsx?', componentNamePattern: '' },
         },
     ],
 })
