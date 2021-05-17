019d5ccbe8c7dee0cb2613e0dcec0568a9232685
Erik Seliger <erikseliger@me.com>
2021-05-17 13:05:54 +0200

    Remove unused file

--- src/parse.js
+++
@@ -1,4 +0,0 @@
-const { readFileSync } = require('fs')
-const { parse } = require('babylon')
-
-console.log(JSON.stringify(parse(readFileSync(process.argv[2], 'utf8'), { sourceType: 'module' }), null, 2))
